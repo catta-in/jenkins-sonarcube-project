55ecf59d0cf69aa255d440b636ae5b5a1895d50f
catta-in <156844053+catta-in@users.noreply.github.com>
2025-07-22 16:45:55 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # demo-playwright-workshop
+Backup e2e project
+
+
+URLs:
+
+1. https://shopdemo-alex-hot.koyeb.app/ (My server)
+2. https://mern-store.onrender.com/ (Backup)
+
+
+Notes:
+
+Debug selectors while pick locator: 
+```
+playwright.getByRole('button', { name: 'Add To Bag' }).elements
+```
---
+++ playwright.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig, devices } from '@playwright/test';
+import { DefaultUserOption } from './fixtures';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig<DefaultUserOption>({
+  testDir: './tests',
+  // testDir: './tests/tests-junior',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  workers: 1,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  // workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['list'],
+    ['html'],
+    // ['./misc/reporters/slowStepReporter.ts'],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://shopdemo-alex-hot.koyeb.app',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: {
+      mode: 'on-first-retry'
+    },
+    headless: process.env.CI ? true : false,
+  },
+  // globalSetup: require.resolve('./misc/cacheWarmer.ts'),
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        // viewport: { width: 1920, height: 1080 },
+        defaultUser: {
+          email: 'xotabu4@gmail.com',
+          password: 'xotabu4@gmail.com'
+        }
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+  ],
+});
